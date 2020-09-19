252e3ce0eec72a1c16bbc6dc0b80208fbfc909f9
Garlov <oe.nordli@gmail.com>
2020-09-19 20:55:57 +0200

    oops, add file

---
+++ src/components/entities/hasCollider.js
@@ -0,0 +1,39 @@
+import gameConfig from 'configs/gameConfig';
+import store from 'root/store';
+
+const hasCollider = function hasColliderFunc(state, colliderType) {
+    function __constructor() {
+        store.colliders.push(state);
+    }
+
+    function update(time) {
+        if (colliderType === gameConfig.COLLIDERS.PLAYER) {
+            const otherColliders = store.colliders.filter(c => c.colliderType !== state.colliderType);
+            const radius = (state.getSprite().height / 2) * state.getScaleY();
+            const position = state.getPosition();
+            otherColliders.forEach((collider) => {
+                const colliderRadius = (collider.getSprite().height / 2) * collider.getScaleY();
+                const colliderPosition = collider.getPosition();
+                if (position.dist(colliderPosition) < colliderRadius + radius) {
+                    console.log('dead');
+                }
+            });
+        }
+        return time;
+    }
+
+    function destroy() {
+        store.colliders.splice(store.colliders.indexOf(state), 1);
+    }
+
+    return {
+        // props
+        colliderType,
+        // methods
+        __constructor,
+        update,
+        destroy,
+    };
+};
+
+export default hasCollider;
